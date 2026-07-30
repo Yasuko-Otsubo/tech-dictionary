8b2b2ba32f7cefe27c4d603727901aefc5f3aa57
yasuko <y.otsubo04@gmail.com>
2026-07-30 23:59:11 +0900

    修正

--- app/_libs/prisma.ts
+++ app/_libs/prisma.ts
@@ -1,8 +1,14 @@
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../generated/prisma/client";
 
-const adapter = new PrismaBetterSqlite3({
-  url: "file:./dev.db"
-})
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma = new PrismaClient({ adapter})
+const adapter = new PrismaBetterSqlite3({ url: "file:./dev.db" });
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
