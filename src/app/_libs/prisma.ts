f1fecf9bc52ad769131db339f910e4f1cfd611cb
yasuko <y.otsubo04@gmail.com>
2026-08-11 20:56:55 +0900

    Prismaの設定をsupabase上のDBに反映

--- src/app/_libs/prisma.ts
+++ src/app/_libs/prisma.ts
@@ -1,14 +1,20 @@
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
-
+/*
+SQLiteからPosgtresQLに変更により削除
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
+*/
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
-const adapter = new PrismaBetterSqlite3({ url: "file:./dev.db" });
 
+export const prisma = new PrismaClient({ adapter })
+/*
+SQLiteからPosgtresQLに変更により削除
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
+*/
