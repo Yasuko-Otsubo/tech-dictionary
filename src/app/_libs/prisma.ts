e77e26c65dfdeaa0c872f360b2bf2fca01e167c8
yasuko <y.otsubo04@gmail.com>
2026-08-11 21:37:39 +0900

    generatedの階層変更による修正

--- src/app/_libs/prisma.ts
+++ src/app/_libs/prisma.ts
@@ -1,15 +1,20 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@/app/generated/prisma/client"; //ここでgenerateされた成果物を読み込んでる
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL
+});
+
+
+export const prisma = new PrismaClient({ adapter })
+
+
 /*
 SQLiteからPosgtresQLに変更により削除
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 */
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-
-
-export const prisma = new PrismaClient({ adapter })
 /*
 SQLiteからPosgtresQLに変更により削除
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
