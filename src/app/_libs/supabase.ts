b6bcf880725cb429c70189e89283e67a4e6c5d5f
yasuko <y.otsubo04@gmail.com>
2026-08-12 23:33:06 +0900

    ブラウザ用クライアント

--- src/app/_libs/supabase.ts
+++ src/app/_libs/supabase.ts
@@ -1,6 +1,18 @@
-import { createClient } from "@supabase/supabase-js";
+//ブラウザ用クライアント
+//ログイン、OTP、パスワード再設定、updateUser() など、クライアント側で使う
 
-export const supabase = createClient(
+import { createBrowserClient } from "@supabase/ssr";
+
+//ブラウザ用のクライアントを作る関数」を読みこむ
+export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-)
+  {
+    auth: {
+      flowType: "pkce", //サーバー側でログイン状態を確認できるようになる
+      detectSessionInUrl: true,
+      persistSession: true, //ログイン情報をブラウザに保存するかどうか
+      autoRefreshToken: true, //アクセストークンの期限が切れそうになったら自動で更新するかどうか
+    },
+  },
+);
