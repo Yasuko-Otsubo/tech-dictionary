defb7fa61034750600c8b61985b97d34c514925a
yasuko <y.otsubo04@gmail.com>
2026-08-25 22:06:20 +0900

    新規登録ボタンなどの追加

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -15,12 +15,23 @@ export default async function TermListPage() {
   });
 
   return (
-    <ul>
-      {terms.map((term) => (
-        <li key={term.id}>
-          <Link href={`/terms/${term.id}`}>{term.itemName}</Link>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <div className="flex justify-between items-center m-2">
+        <h1>用語一覧</h1>
+        <Link href="/terms/new">
+          <button className="border-1">新規登録</button>
+        </Link>
+      </div>
+
+      <hr className="mt-2 mb-2" />
+
+      <ul>
+        {terms.map((term) => (
+          <li key={term.id}>
+            <Link href={`/terms/${term.id}`}>{term.itemName}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
