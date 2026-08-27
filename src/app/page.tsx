770ab9c2f110114c984d1191903a57260c4cbdf0
yasuko <y.otsubo04@gmail.com>
2026-08-27 18:53:55 +0900

    並び替えの実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,22 @@ import { getCurrentProfile } from "./_libs/getCurrentProfile";
 import { prisma } from "./_libs/prisma";
 import Link from "next/link";
 
-export default async function TermListPage() {
+export default async function TermListPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ sort?: string }>;
+}) {
   const profile = await getCurrentProfile();
 
   if (!profile) {
     redirect("/login");
   }
 
+  const { sort } = await searchParams;
+
   const terms = await prisma.terms.findMany({
     where: { userId: profile.id },
+    orderBy: sort === "name" ? { itemName: "asc" } : { createdAt: "asc" },
   });
 
   return (
@@ -21,6 +28,10 @@ export default async function TermListPage() {
         <Link href="/terms/new">
           <button className="border-1">新規登録</button>
         </Link>
+      </div>
+      <div>
+        <Link href="/">登録順</Link>
+        <Link href="/?sort=name">あいうえお順</Link>
       </div>
 
       <hr className="mt-2 mb-2" />
