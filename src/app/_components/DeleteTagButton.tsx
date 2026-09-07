8a165db63b7acc5293561549b86d7e7f726e4c17
yasuko <y.otsubo04@gmail.com>
2026-09-07 23:22:56 +0900

    タグの削除処理を実装

---
+++ src/app/_components/DeleteTagButton.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { deleteTag } from "@/app/_libs/_actions/tags";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react"
+
+export default function DeleteTagButton({ id } : { id: number }) {
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+
+  const handleDelete = () => {
+    if (!confirm("このタグを削除しますか？")) return;
+    startTransition(async () => {
+      await deleteTag(id);
+      router.refresh();
+    });
+  };
+
+  return (
+    <button
+    type="button"
+    onClick={handleDelete}
+    disabled={isPending}
+    className="ml-1 text-[#1F2937] hover:text-red-600">
+      ✖
+    </button>
+  )
+}
