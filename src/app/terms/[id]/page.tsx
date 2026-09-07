7aa46594d695ff09ed4c808a974a4b86f96185a4
yasuko <y.otsubo04@gmail.com>
2026-09-07 22:05:46 +0900

    詳細ページのcss実装 それぞれにラベルも追加

--- src/app/terms/[id]/page.tsx
+++ src/app/terms/[id]/page.tsx
@@ -29,19 +29,36 @@ export default async function DetailTerm({
 
   return (
     <div>
-      <Link href={`/terms/${term.id}/edit`}>編集</Link>
-      <DeleteTermButton id={term.id} />
-      <div>
-        <h1>{term.itemName}</h1>
-        {term.itemContent && <p>{term.itemContent}</p>}
-        {term.image && <img src={term.image} alt={term.itemName} />}
-
+      <div className="flex justify-between items-center p-2">
+        <Link href={`/terms/${term.id}/edit`}>編集</Link>
+        <DeleteTermButton id={term.id} />
+      </div>
+      <div className="p-2">
+        <h1 className="text-2xl font-bold mb-2">{term.itemName}</h1>
+        {term.itemContent && (
+          <div className="mb-4">
+            <p className="text-sm text-gray-400 mb-1">説明</p>
+            <p className="text-gray-700">{term.itemContent}</p>
+          </div>
+        )}
+        {term.image && (
+            <div className="mb-4">
+                <p className="text-sm text-gray-400 mb-1">画像</p>
+          <img src={term.image} alt={term.itemName} className="max-w-full rounded-sm mb-4" />
+          </div>
+        )}
+        {term.referenceUrls.length > 0 && (
+          <div className="mb-4">
+            <p className="text-sm text-gray-400 mb-1">参考URL</p>
         {term.referenceUrls.map((url, index) => (
-          <a key={index} href={url} target="_blank" rel="noopener noreferrer">
+          <a key={index} href={url} target="_blank" rel="noopener noreferrer" className="block text-[#1F2937] underline hover:text-[#6BA6CC] mb-1">
             参考リンク{index + 1}
           </a>
         ))}
       </div>
+              )}
+
+    </div>
     </div>
   );
 }
