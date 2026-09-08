d90d0a73a60f52e6acbda9f11bd7b684d713c19f
yasuko <y.otsubo04@gmail.com>
2026-09-08 21:04:15 +0900

    一覧ページへのリンク

--- src/app/terms/new/page.tsx
+++ src/app/terms/new/page.tsx
@@ -2,6 +2,7 @@ import { getCurrentProfile } from "@/app/_libs/getCurrentProfile";
 import { redirect } from "next/navigation";
 import NewTermForm from "./_components/NewTermForm";
 import { prisma } from "@/app/_libs/prisma";
+import Link from "next/link";
 
 export default async function NewTermPage() {
   const profile = await getCurrentProfile();
@@ -16,5 +17,12 @@ export default async function NewTermPage() {
     },
   });
 
-  return <NewTermForm tags={tags} />;
+  return (
+    <div>
+      <Link href="/" className="text-sm text-gray-400 hover:text-[#1F2937]">
+        ←　一覧に戻る
+      </Link>
+      <NewTermForm tags={tags} />;
+    </div>
+  );
 }
