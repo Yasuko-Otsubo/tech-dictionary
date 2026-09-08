81f81cec77f1ec2af8bf1f5dd4f65b8d56094796
yasuko <y.otsubo04@gmail.com>
2026-09-08 22:55:33 +0900

    カラーの順番入れ替え

--- src/app/_libs/tagColors.ts
+++ src/app/_libs/tagColors.ts
@@ -5,9 +5,9 @@ export type TagColor = {
 
 export const TAG_COLORS: TagColor[] = [
   { name: "pink", value: "#e4acb8" },
-  { name: "blue", value: "#98bde0" },
-  { name: "green", value: "#aee6b1" },
   { name: "yellow", value: "#e6ec94" },
+  { name: "green", value: "#aee6b1" },
+  { name: "blue", value: "#98bde0" },
   { name: "purple", value: "#baaacf" },
   { name: "gray", value: "#c3c3c3" },
 ];
