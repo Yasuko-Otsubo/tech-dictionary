5280155cf8b95999d3ab9bf6395e3c80ae1efcf0
yasuko <y.otsubo04@gmail.com>
2026-09-10 22:36:22 +0900

    togglememorizedの追加

--- src/app/_libs/_actions/terms.ts
+++ src/app/_libs/_actions/terms.ts
@@ -110,3 +110,24 @@ export async function updateTerm(
 
   return { success: true };
 }
+
+export async function toggleMemorized(id: number, isMemorized: boolean) {
+  const profile = await getCurrentProfile();
+
+  if (!profile ) {
+    redirect("/login");
+  }
+
+  const term = await prisma.terms.findFirst({
+    where: { id, userId: profile.id },
+  });
+
+  if (!term) {
+    return;
+  }
+
+  await prisma.terms.update({
+    where: { id },
+    data: { isMemorized: !isMemorized },
+  });
+}
